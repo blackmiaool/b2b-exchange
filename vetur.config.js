4806a97042dcdc8139e9fe9f412f0be777fd90f4
blackmiaool <blackmiaool@gmail.com>
2022-04-17 22:58:11 +0800

    add web dir

--- vetur.config.js
+++ vetur.config.js
@@ -13,13 +13,13 @@ module.exports = {
             // **required**
             // Where is your project?
             // It is relative to `vetur.config.js`.
-            root: "./",
-            tsconfig: "./tsconfig.json",
+            root: "./web",
+            tsconfig: "./web/tsconfig.json",
             // **optional** default: `'package.json'`
             // Where is `package.json` in the project?
             // We use it to determine the version of vue.
             // It is relative to root property.
-            package: "./package.json"
+            package: "./web/package.json"
         }
     ]
 };
