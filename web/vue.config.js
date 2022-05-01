81dc41610eb6566750dc830ac7728a2075090d41
blackmiaool <blackmiaool@gmail.com>
2022-05-01 22:03:47 +0800

    add docs

--- web/vue.config.js
+++ web/vue.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
     transpileDependencies: true,
-
+    publicPath: "./",
+    outputDir: "../docs",
     devServer: {
         port: 20395,
         host: "0.0.0.0"
